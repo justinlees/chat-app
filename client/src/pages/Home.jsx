eab4b7c5c30b176b0740820ce3902362da027776
justinlees <121879669+justinlees@users.noreply.github.com>
2025-09-05 01:54:08 +0530

    Home page Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">Me_Chat</h1>
+
+      <a href="/signUp">Sign Up</a>
+      <a href="/login">Login</a>
     </div>
   );
 };

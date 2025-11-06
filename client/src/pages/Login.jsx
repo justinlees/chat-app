a4433b92d2a409664b5f1da46c359509a9d2d57b
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-06 18:27:07 +0530

    addedLoader

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const checkToken = async (e) => {
       try {
+        setIsLoading(true);
         const response = await fetch(
           `${import.meta.env.VITE_BASE_URL || "http://localhost:5000"}/login`,
           {
@@ -16,6 +18,7 @@ const Login = () => {
         );
         const data = await response.json();
         if (data.message === "User Token exist") {
+          setIsLoading(false);
           window.location.href = `/user/${data.user._id}`;
         }
       } catch (error) {
@@ -32,6 +35,7 @@ const Login = () => {
       email: e.target.Email.value,
       password: e.target.Password.value,
     };
+    setIsLoading(true);
     const response = await fetch(
       `${import.meta.env.VITE_BASE_URL || "http://localhost:5000"}/login`,
       {
@@ -45,53 +49,63 @@ const Login = () => {
     );
     const data = await response.json();
     if (data.message === "Login success") {
+      setIsLoading(false);
       window.location.href = `/user/${data.user._id}`;
     } else {
+      setIsLoading(false);
       alert(data.message);
       window.location.href = "/login";
     }
     e.target.reset();
   };
-  return (
-    <div className="flex flex-col items-center justify-center h-screen ">
-      <h1 className="text-3xl mb-2 font-bold">Login</h1>
-      <form
-        className="flex flex-col item-center justify-evenly gap-5 bg-gray-800 p-12 rounded h-1/2 "
-        method="POST"
-        onSubmit={handleSubmit}
-      >
-        <input
-          className="border border-gray-300 rounded p-2 text-gray-100 w-72"
-          type="email"
-          placeholder="Enter your Email"
-          name="Email"
-          required
-        />
-        <input
-          className="border border-gray-300 rounded p-2 text-gray-100"
-          type="password"
-          placeholder="Enter your password"
-          name="Password"
-          required
-        />
-        <button
-          type="submit"
-          className="text-white hover:bg-gray-600 p-2 rounded cursor-pointer"
+
+  if (isLoading)
+    return (
+      <div className="loadPage">
+        <span className="mainLoader"></span>
+      </div>
+    );
+  else
+    return (
+      <div className="flex flex-col items-center justify-center h-screen ">
+        <h1 className="text-3xl mb-2 font-bold">Login</h1>
+        <form
+          className="flex flex-col item-center justify-evenly gap-5 bg-gray-800 p-12 rounded h-1/2 "
+          method="POST"
+          onSubmit={handleSubmit}
         >
-          Login
-        </button>
-        <p className="text-gray-400">
-          Don't have an account?
-          <a href="signUp" className="cursor-pointer">
-            Register
+          <input
+            className="border border-gray-300 rounded p-2 text-gray-100 w-72"
+            type="email"
+            placeholder="Enter your Email"
+            name="Email"
+            required
+          />
+          <input
+            className="border border-gray-300 rounded p-2 text-gray-100"
+            type="password"
+            placeholder="Enter your password"
+            name="Password"
+            required
+          />
+          <button
+            type="submit"
+            className="text-white hover:bg-gray-600 p-2 rounded cursor-pointer"
+          >
+            Login
+          </button>
+          <p className="text-gray-400">
+            Don't have an account?
+            <a href="signUp" className="cursor-pointer">
+              Register
+            </a>
+          </p>
+          <a href="forgot-password" className="cursor-pointer text-gray-400">
+            Forgot Password?
           </a>
-        </p>
-        <a href="forgot-password" className="cursor-pointer text-gray-400">
-          Forgot Password?
-        </a>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
 };
 
 export default Login;

6a8864df0e2b533623afb421ac8e193693183ec7
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-06 18:34:43 +0530

    addedLoader

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -5,8 +5,10 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [mobile, setMobile] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const response = await fetch(
       `${import.meta.env.VITE_BASE_URL || "http://localhost:5000"}/signUp`,
       {
@@ -26,88 +28,101 @@ const SignUp = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.message === "User created successfully") {
+          setIsLoading(false);
           alert("User created successfully");
           window.location.href = `/user/${data.user._id}`;
         }
         if (data.message === "User already exists") {
+          setIsLoading(false);
           alert("User already exists");
         }
         if (data.message === "All fields are required") {
+          setIsLoading(false);
           alert("All fields are required");
         }
         if (data.message === "Password must be of at least 6 characters") {
+          setIsLoading(false);
           alert("Password must be of at least 6 characters");
         }
 
         if (data.message === "User creation failed") {
+          setIsLoading(false);
           alert("User creation failed");
         }
 
         if (data.message === "Internal server error") {
+          setIsLoading(false);
           alert("Internal server error");
         }
       });
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl mb-2 font-bold">SignUp</h1>
-      <form
-        className="flex flex-col item-center gap-6 bg-gray-800 p-12 rounded "
-        method="POST"
-        // // action={`http://localhost:${process.env.PORT || 5000}/signUp`}
-        // action="/signUp"
-        onSubmit={handleSubmit}
-      >
-        <input
-          className="border border-gray-300 rounded p-2 text-gray-100 w-72"
-          type="text"
-          placeholder="Enter your Full Name"
-          name="name"
-          required
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <input
-          className="border border-gray-300 rounded p-2 text-gray-100 w-72"
-          type="email"
-          placeholder="Enter your Email"
-          name="email"
-          required
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          className="border border-gray-300 rounded p-2 text-gray-100 w-72"
-          type="password"
-          placeholder="Enter your Password"
-          name="password"
-          required
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <input
-          className="border border-gray-300 rounded p-2 text-gray-100 w-72"
-          type="tel"
-          placeholder="Enter your Mobile Number"
-          name="mobile"
-          required
-          onChange={(e) => {
-            setMobile(e.target.value);
-          }}
-        />
-        <button
-          type="submit"
-          className="text-white hover:bg-gray-600 p-2 rounded cursor-pointer"
+  if (isLoading)
+    return (
+      <div className="loadPage">
+        <span className="mainLoader"></span>
+      </div>
+    );
+  else
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-3xl mb-2 font-bold">SignUp</h1>
+        <form
+          className="flex flex-col item-center gap-6 bg-gray-800 p-12 rounded "
+          method="POST"
+          // // action={`http://localhost:${process.env.PORT || 5000}/signUp`}
+          // action="/signUp"
+          onSubmit={handleSubmit}
         >
-          Register
-        </button>
-      </form>
-    </div>
-  );
+          <input
+            className="border border-gray-300 rounded p-2 text-gray-100 w-72"
+            type="text"
+            placeholder="Enter your Full Name"
+            name="name"
+            required
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <input
+            className="border border-gray-300 rounded p-2 text-gray-100 w-72"
+            type="email"
+            placeholder="Enter your Email"
+            name="email"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            className="border border-gray-300 rounded p-2 text-gray-100 w-72"
+            type="password"
+            placeholder="Enter your Password"
+            name="password"
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <input
+            className="border border-gray-300 rounded p-2 text-gray-100 w-72"
+            type="tel"
+            placeholder="Enter your Mobile Number"
+            name="mobile"
+            required
+            onChange={(e) => {
+              setMobile(e.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            className="text-white hover:bg-gray-600 p-2 rounded cursor-pointer"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    );
 };
 
 export default SignUp;
